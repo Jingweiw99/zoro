c9c530a97459d0b58a82f572a7545c87b4f7a963
wangjingwei <wangjingwei0@foxmail.com>
2023-07-16 18:13:57 +0800

    feat: 前端校验表单，以减少服务器请求

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,13 +3,18 @@
     <el-row>
       <el-col :span="12" :xs="0"></el-col>
       <el-col :span="12" :xs="24">
-        <el-form class="login_form" :model="userForm">
+        <el-form
+          class="login_form"
+          :model="userForm"
+          :rules="rules"
+          ref="loginForms"
+        >
           <h1>hello</h1>
           <h2>欢迎来到硅谷甄选</h2>
-          <el-form-item>
+          <el-form-item prop="username">
             <el-input v-model="userForm.username" prefix-icon="User"></el-input>
           </el-form-item>
-          <el-form-item>
+          <el-form-item prop="password">
             <el-input
               type="password"
               v-model="userForm.password"
@@ -42,26 +47,67 @@ const userForm = reactive({ username: 'admin', password: '111111' })
 const loginStore = useLoginStore()
 const router = useRouter()
 // 登录业务
+const loginForms = ref()
 const loading = ref(false)
 const login = async () => {
-  loading.value = true
-  // 通知仓库发送请求。 登陆成功，跳转到home页面。 登录失败，提示登录失败了。
   try {
-    await loginStore.userLogin(userForm)
-    router.push('/')
-    loading.value = false
-    ElNotification({
-      type: 'success',
-      message: '欢迎回来',
-      title: `HI,${getTime()}好`,
-    })
-  } catch (error: any) {
-    loading.value = false
-    ElNotification({
-      type: 'error',
-      message: error.message,
-    })
+    await loginForms.value.validate()
+    loading.value = true
+    // 通知仓库发送请求。 登陆成功，跳转到home页面。 登录失败，提示登录失败了。
+    try {
+      await loginStore.userLogin(userForm)
+      router.push('/')
+      loading.value = false
+      ElNotification({
+        type: 'success',
+        message: '欢迎回来',
+        title: `HI,${getTime()}好`,
+      })
+    } catch (error: any) {
+      loading.value = false
+      ElNotification({
+        type: 'error',
+        message: error.message,
+      })
+    }
+  } catch (validateError: any) {
+    const { username, password } = validateError
+    console.error(
+      username && username[0].message,
+      password && password[0].message,
+    )
   }
+}
+
+// 表单校验
+// const passwordPass = (rule: any, value: any, cb: any) => {
+//   // 自定义校验规则可以结合正则表达式，失败也需要调用并传入错误信息。
+//   if (value.length >= 6) {
+//     cb()
+//   } else {
+//     cb(new Error('密码长度至少六位'))
+//   }
+// }
+const rules = {
+  username: [
+    // change文本发生变化，blur失去焦点
+    { required: true, message: '用户名不能为空', trigger: 'blur' },
+    {
+      min: 5,
+      max: 10,
+      message: '用户名长度必须在5到10之间',
+      trigger: 'change',
+    },
+  ],
+  password: [
+    {
+      min: 6,
+      max: 12,
+      message: '密码长度必须在6到12之间',
+      trigger: 'change',
+    },
+    // { validator: passwordPass, trigger: 'change' },
+  ],
 }
 </script>
 
